46225520ffe2ab755a939b51afacf92592b849ba
channavi <101348717+channavi@users.noreply.github.com>
2023-10-06 11:11:25 +0900

    channavi

--- AnimalinRoad/Assets/PlayerMove.cs
+++ AnimalinRoad/Assets/PlayerMove.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class carMove : MonoBehaviour
+public class PlayerMove : MonoBehaviour
 {
-
+    // Start is called before the first frame update
     void Start()
     {
         
@@ -13,6 +13,6 @@ public class carMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 }
